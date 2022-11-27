987a71aba8579470c8e71226361f1aea112f5839
Oliver Lauridsen <olidude2.0@gmail.com>
2022-11-28 00:15:59 +0100

    removes show admin bar line

--- functions.php
+++ functions.php
@@ -3,4 +3,3 @@ require_once dirname( __FILE__ ) . '/inc/enqueues/backend/styles.php';
 require_once dirname( __FILE__ ) . '/inc/enqueues/backend/scripts.php';
 require_once dirname( __FILE__ ) . '/inc/enqueues/frontend/styles.php';
 require_once dirname( __FILE__ ) . '/inc/enqueues/frontend/scripts.php';
-show_admin_bar(true);
